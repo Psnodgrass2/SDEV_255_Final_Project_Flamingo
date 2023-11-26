362e3c7b1c8aace0bc60afbf7723bec3a3cddfce
Phillip Snodgrass <psnodgrass2@ivytech.edu>
2023-11-26 16:57:42 -0500

    update

--- src/components/MessagesText.vue
+++ src/components/MessagesText.vue
@@ -1,18 +1,32 @@
 <template>
     <div>
-        <h4>Messages</h4>
-        <ul v-for="message in messages" :key="message.id">
-            <li>{{message}}</li>
-        </ul>
+        <h4 class="text-h4">Messages</h4>
+<v-card class="mx-auto" max-width="300">
+    <v-list :items="messages">
+       
+    </v-list>
+</v-card>
+        
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            messages: ["hello", "hi", "it's working"]
-        }
-    }
-}
+  export default {
+    data: () => ({
+      messages: [
+        {
+          title: 'hi',
+          value: 1,
+        },
+        {
+          title: 'hello',
+          value: 2,
+        },
+        {
+          title: "it's working",
+          value: 3,
+        },
+      ],
+    }),
+  }
 </script>
