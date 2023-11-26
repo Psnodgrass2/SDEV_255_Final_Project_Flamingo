7098c4829ce9aaa7d3a077d087e112ca7e256d40
Phillip Snodgrass <psnodgrass2@ivytech.edu>
2023-11-26 16:20:21 -0500

    made changes following tutorial

---
+++ src/components/MessagesText.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h4>Messages</h4>
+        <ul v-for="message in messages" :key="message.id">
+            <li>{{message}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages: ["hello", "hi", "it's working"]
+        }
+    }
+}
+</script>
