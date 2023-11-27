56f52ac62ce6f6bbf3505d00d53f9538c29407d0
Adam L. <144614242+AStellarScene@users.noreply.github.com>
2023-11-26 21:58:20 -0500

    Modify Vue config to run on Replit.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: 'all',
+  },
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
