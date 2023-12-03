7afd9305b2433689dc6343b5c28f76a81fe0c865
Adam L <144614242+AStellarScene@users.noreply.github.com>
2023-12-03 18:42:54 -0500

    Update App.vue to display a dynamic hamburger menu for the sidebar.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
     <v-layout class="rounded rounded-md">
-      <router-link to="/"><v-app-bar title="Course Modification Application"></v-app-bar></router-link>
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <router-link to="/"><v-toolbar-title>Course Modification Application</v-toolbar-title></router-link>
+      </v-app-bar>
 
       <v-main style="min-height: 1000px">
-        <v-navigation-drawer location="left">
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" to="/"></v-list-item>
-          <v-list-item prepend-icon="mdi-chair-rolling" title="Courses" value="courses" to="/courses"></v-list-item>
-          <v-list-item prepend-icon="mdi-folder-plus" title="Create Course" value="create-course" to="/create/course"></v-list-item>
-          <v-list-item prepend-icon="mdi-account" title="My Account" value="account" to="/account"></v-list-item>
-          <v-list-item prepend-icon="mdi-login" title="Login" value="login" to="/login"></v-list-item>
-          <v-list-item prepend-icon="mdi-logout" title="Logout" value="logout" to="/logout"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+        <v-navigation-drawer v-model="drawer" location="left">
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-home-city" title="Home" value="home" to="/"></v-list-item>
+            <v-list-item prepend-icon="mdi-chair-rolling" title="Courses" value="courses" to="/courses"></v-list-item>
+            <v-list-item prepend-icon="mdi-folder-plus" title="Create Course" value="create-course" to="/create/course"></v-list-item>
+            <v-list-item prepend-icon="mdi-account" title="My Account" value="account" to="/account"></v-list-item>
+            <v-list-item prepend-icon="mdi-login" title="Login" value="login" to="/login"></v-list-item>
+            <v-list-item prepend-icon="mdi-logout" title="Logout" value="logout" to="/logout"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
 
         <router-view></router-view>
       </v-main>
@@ -29,7 +32,7 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
